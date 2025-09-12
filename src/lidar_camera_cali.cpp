f5f5940bbd115b5a40ef319d499bbb93d13490cc
Yixiang Gao <yixiangg310573@gmail.com>
2025-09-12 00:10:01 +0000

    clean up

--- src/lidar_camera_cali.cpp
+++ src/lidar_camera_cali.cpp
@@ -11,8 +11,6 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/ModelCoefficients.h>
 #include <pcl/point_types.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/filters/extract_indices.h>
